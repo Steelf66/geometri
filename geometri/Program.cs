c28e27f595ac9e2ab76c90d069c3b03d4aa7ec28
stef5924 <stef5924@ZBC-S-STEF5924>
2021-08-31 13:33:54 +0200

    Add project files.

---
+++ geometri/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace geometri
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square1 = new Square();
+            square1.A = 14;
+
+            Console.WriteLine(square1.perimeter());
+            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine(square1.area());
+            Console.WriteLine();
+            Console.WriteLine();
+           
+
+        }
+    }
+}
---
+++ geometri/Square.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometri
+{
+    public class Square
+    {
+        private int a;
+        public int A
+        {
+            get
+            {
+                return this.a;
+            }
+            set
+            {
+                this.a = value;
+            }
+        }
+        public int perimeter()
+        {
+            return this.a * 4;
+        }
+        public int area()
+        {
+            return this.a * a;
+        }
+        public Square()
+        {
+
+        }
+    }
+}
